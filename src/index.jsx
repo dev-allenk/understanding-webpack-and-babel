1cde7a3b6f5cbe7791a8fe3b785ef6e270e8ea0c
allenk <dev.allenk@gmail.com>
2020-05-02 00:47:54 +0900

    edit index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,11 @@
+// 1. 바닐라 코드
+import component from "./component";
+
+document.querySelector("#root").appendChild(component());
+
+// 2. 리액트 코드
 // import React from "react";
 // import { render } from "react-dom";
 // import App from "./App";
 
 // render(<App />, document.querySelector("#root"));
-
-import component from "./component";
-
-document.querySelector("#root").appendChild(component());
